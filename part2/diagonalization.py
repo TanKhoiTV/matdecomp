1edf9f863f6d4d946f761602d98407bad37f179e
NhatHoang101 <nhathoang2k76@gmail.com>
2026-03-26 15:50:21 +0700

    feat: add diagonaliztion function (#58)

---
+++ part2/diagonalization.py
@@ -0,0 +1,31 @@
+def diagonalize(A):
+    """
+    Chéo hóa ma trận vuông A sao cho:
+        A = P D P^-1
+
+    trong đó:
+    - D: là ma trận đường chéo chứa các giá trị riêng,
+    - P: chứa các vector riêng tương ứng dưới dạng các cột,
+    - P_inv: là ma trận nghịch đảo của P.
+
+    Điều kiện để chéo hóa được:
+    - A phải là ma trận vuông.
+    - A chéo hóa được khi nó có đủ số lượng vector riêng độc lập tuyến tính
+    (tương đương tổng bội hình học bằng kích thước ma trận).
+
+    Giá trị trả về:
+        tuple: (P, D, P_inv) 
+    P: ma trận chứa các vector riêng dưới dạng các cột
+    D: ma trận đường chéo chứa các giá trị riêng
+    P_inv: ma trận nghịch đảo của P
+    """
+    # Method: Sử dụng phương pháp lặp lũy thừa để tính toán giá trị riêng và vector riêng
+
+    # TODO:
+    # - Kiểm tra A có phải ma trận vuông hay không
+    # - Cài đặt tìm giá trị riêng và vector riêng
+    # - Xây dựng ma trận P, D và tính P^-1
+
+    # Chưa cài đặt (sẽ hoàn thiện ở Sprint 2)
+
+    return None, None, None 
