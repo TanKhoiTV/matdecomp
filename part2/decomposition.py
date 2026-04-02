fcc1c05d85109708c1910326a109972787647056
Dat Le <thanhdat952006@gmail.com>
2026-04-02 16:29:43 +0700

    feat: implemented functions for calculating eigenvalues

--- part2/decomposition.py
+++ part2/decomposition.py
@@ -1,5 +1,60 @@
 from typing import List, Tuple
+from math import atan2, sin, cos
 
+
+def transpose(A):
+    return [[A[j][i] for j in range(len(A))] for i in range(len(A[0]))]
+
+def multiply(A, B):
+    result = [[0.0 for _ in range(len(B[0]))] for _ in range(len(A[0]))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] = A[i][k] + B[k][j]
+    return result
+
+def get_identity_matrix(n):
+    return [[1.0 if i == j else 0.0 for j in range(n)] for i in range(n)]
+
+def jacobi_eigenvalues(S, error_tolerance = 1e-10):
+    n = len(S)
+    V = get_identity_matrix(n)
+    max_iterations = 100
+
+    for _ in range(max_iterations):
+        max_val = 0
+        p, q = 0, 1
+        for i in range(n):
+            for j in range(i + 1, n):
+                if abs(S[i][j]) > max_val:
+                    max_val = abs(S[i][j])
+                    p, q = i, j
+
+        if max_val < error_tolerance:
+            break
+
+        phi = 0.5 * atan2(2 * S[p][q], S[q][q] - S[p][p])
+        c, s = cos(phi), sin(phi)
+
+        s_pp, s_qq = S[p][p], S[q][q]
+        S[p][p] = c ** 2 * s_pp - 2 * s * c * S[p][q] + s ** 2 * s_qq
+        S[q][q] = s ** 2 * s_pp + 2 * s * c * S[p][q] + c ** 2 * s_qq
+        S[p][q] = S[q][p] = 0.0
+
+        for i in range(n):
+            if i != p and i != q:
+                s_ip, s_iq = S[i][p], S[i][q]
+                S[i][p] = S[p][i] = c * s_ip - s * s_iq
+                S[i][q] = S[q][i] = s * s_ip + c * s_iq
+        
+        for i in range(n):
+            v_ip, v_iq = V[i][p], V[i][q]
+            V[i][p] = c * v_ip - s * v_iq
+            V[i][q] = s * v_ip + c * v_iq
+    
+    eigenvalues = [S[i][i] for i in range(n)]
+    
+    return eigenvalues, V
 
 def svd_decompose(A: List[List[float]]) -> Tuple[List[List[float]], List[List[float]], List[List[float]]]:
     """
